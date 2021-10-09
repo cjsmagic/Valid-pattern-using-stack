43058f7f43783d965a10cecd5e107b38dd3ddf00
cjsmagic <clarencemerchant1995@gmail.com>
2021-10-09 17:37:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('hello!');
+
+const openings = {
+  '{': '{',
+};
+
+const closings = {
+  '}': '}',
+};
+
+const ex1 = '{}{{}}';
+
+function isValidaPattern(pattern) {
+  const stack = [];
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const preChar = stack[stack.length - 1];
+    if (openings[char]) {
+      // push into stack if opening
+      stack.push(char);
+    } else if (openings[preChar]) {
+      // pop from stack if closing
+      stack.pop();
+    } else {
+      stack.push(char); // in case of irregular pattern keep pushing
+    }
+    console.log(i, stack);
+  }
+  return stack.length === 0 ? 'valid' : 'invalid';
+}
+
+console.log(isValidaPattern(ex1));
